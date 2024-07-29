4b1a1a8ce48a9e39d4b6c6269cbe4ad5b876de76
M.J <147601389+Mohamad-Jamal-J@users.noreply.github.com>
2024-07-30 00:52:24 +0300

    Update RemoveNthNode.java
    Removed comment

--- leetcode_solutions/first_ds_lecture/remove_nth_from_end/RemoveNthNode.java
+++ leetcode_solutions/first_ds_lecture/remove_nth_from_end/RemoveNthNode.java
@@ -1,16 +1,6 @@
 package tasks.first_ds_lecture.remove_nth_from_end;
 
 public class RemoveNthNode {
-   /**
-   motivation:
-   n: position from right, list length is k
-   so k-n: position from left (given k>=n>=1)
-   k-n is the position where we should stop to delete the target node (at k-n+1)
-   approach:
-   using two pointers
-   move one pointer by n steps ahead
-   then move both pointers k-n step ahead (until the first one is at k, and the second is at k-n)
-    **/
    public ListNode removeNthFromEnd(ListNode head, int n) {
       ListNode fast = head, slow = head;
       for(int i=0; i<n ;i++) // move fast n times
@@ -20,7 +10,7 @@ public class RemoveNthNode {
          return head.next;
 
       while(fast.next!=null){
-         // move k-n times
+         // move k-n-1 times
          // until fast becomes at k
          // and slow at k-n (the position before the target node)
          fast = fast.next;
